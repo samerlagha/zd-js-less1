b2eb410bd7ceb58b5108dd9d38bd42949a3e7245
samerlagha <samer_la@mail.ru>
2019-10-25 17:08:56 +0300

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,8 @@
+const userName = 'John';//не должно содержать пробелы
+const greeting = "I'm John";//не может начинаться с цифр
+const _profession = 'Developer';//могут содержать  символы _ 
+const $hobby = 'Walking';      //могут содержать  символы  $
+const favouriteDESSERT = 'cake';//camelCase
+let pet_ = 'dog';//могут содержать  символы _ 
+let superNumber;//Имена переменных могут содержать буквы, цыфры, символы _ и $ 
+superNumber = 17;
