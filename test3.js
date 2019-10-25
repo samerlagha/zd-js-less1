e322f36a85db5368f5eaeaccdcef1cb5811a5db1
samerlagha <samer_la@mail.ru>
2019-10-25 17:10:56 +0300

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,12 @@
+let goodNmber=17;
+let anotherNumber=goodNmber;
+anotherNumber=77;
+console.log(goodNmber);
+console.log(anotherNumber);
+let customer={
+	name:'John',age:17
+}
+let anotherCustomer= customer
+anotherCustomer.age=20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
