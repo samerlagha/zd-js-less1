aae0c5f7744fbe61899cd58423693d7fda129ff8
samerlagha <samer_la@mail.ru>
2019-10-26 11:29:59 +0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -2,7 +2,7 @@ const userName = 'John';//не должно содержать пробелы
 const greeting = "I'm John";//не может начинаться с цифр
 const _profession = 'Developer';//могут содержать  символы _ 
 const $hobby = 'Walking';      //могут содержать  символы  $
-const favouriteDESSERT = 'cake';//camelCase
+const favouriteDessert='cake';//camelCase
 let pet_ = 'dog';//могут содержать  символы _ 
 let superNumber;//Имена переменных могут содержать буквы, цыфры, символы _ и $ 
 superNumber = 17;
